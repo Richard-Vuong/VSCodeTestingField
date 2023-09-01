2472626f217f67364ca31eee8dc93f7c2ad677b0
Richard-Vuong <Richard_Vuong@outlook.com>
2023-09-01 11:42:43 +0100

    exponents

--- Python.py
+++ Python.py
@@ -1,3 +1,19 @@
 #Learning about print arguments (sep and end)
-print("    *", "   * *", "  *   *"," *     *", "***   ***","  *   *","  *   *", "  *****", sep = "\n", end = "")
+def arrow():
+    print("    *", "   * *", "  *   *"," *     *", "***   ***","  *   *","  *   *", "  *****", sep = "\n", end = "")
 
+#learning about literals 
+
+#this is an octal number with a decmial value equal to 83
+def octal83():
+    print("\n",0o123, sep="")
+
+#0x123 is a hexadecimal number with a decimal = 291
+def hd291():
+    print(0x123)
+
+#can use E for exponent, e.g. 3E4 = 3 * 10^4, SIMILARLY, 3E-4 = 3 * 10^-4
+def threeem4():
+    print(3E-4)
+
+threeem4()
