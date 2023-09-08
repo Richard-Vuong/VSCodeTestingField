28e296e16aa3db0b38fea428c1bd162ed63d4976
Richard-Vuong <Richard_Vuong@outlook.com>
2023-09-08 18:46:43 +0100

    Factorial Function

--- Python.py
+++ Python.py
@@ -245,3 +245,25 @@ print(a)
 
 result outputs 2 then 2 again.
 '''
+
+
+
+''' FACTORIAL FUNCTION
+
+def factorial_function(n):
+    if n < 0:
+        return None
+    if n < 2:
+        return 1
+    
+    product = 1
+    for i in range(2, n + 1):
+        product *= i
+    return product
+
+
+for n in range(1, 6):  # testing
+    print(n, factorial_function(n))
+
+
+'''
